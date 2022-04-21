1cebfc4a52d0cad0ada393023c28ae2261895906
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-04-21 15:07:21 +0200

    feat: add data from server

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import thunk from "redux-thunk"
 import tableReducer from "../features/tableSlice"
 
 export default configureStore({
 	reducer: {
 		table: tableReducer,
 	},
+	middleware: [thunk],
 })
--- src/features/tableSlice.js
+++ src/features/tableSlice.js
@@ -1,9 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
+import { API_BASE_URL } from "../constants"
+
+export const getTableData = createAsyncThunk("getTableData", () =>
+	axios
+		.get(API_BASE_URL)
+		.then(res => {
+			return res.data
+		})
+		.catch(err => console.error(err))
+)
 
 export const tableSlice = createSlice({
 	name: "table",
-	initialState: {},
+	initialState: {
+		tableData: [],
+	},
 	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(getTableData.fulfilled, (state, action) => {
+				state.tableData = action.payload
+			})
+			.addCase(getTableData.rejected, (state, action) => {
+				console.log(state, action)
+			})
+	},
 })
 
 export default tableSlice.reducer
